a3b5a8c2bbed5cc719705536cba83b9911e5bfe3
Philip W Fowler <philipwfowler@users.noreply.github.com>
2022-01-25 09:16:52 +0000

    Python3.7

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent"],
-    python_requires='>=3.8',
+    python_requires='>=3.7',
     install_requires=[
         "numpy",
         "matplotlib",
